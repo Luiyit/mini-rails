c5dcd9a1fa036346b85e2adffff119793fcb3422
Luiyit Hernandez <luiyit.hernandez@widget-pc.com>
2020-05-30 14:54:27 -0500

    Mini Active Record done!

---
+++ test/ai_blog/db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Post.create([
+  { 
+    title: 'Star Wars', 
+    body: 'Consequat Lorem non ut dolore excepteur in ad non fugiat labore veniam qui magna magna. Ex occaecat cillum eu et veniam. Enim incididunt in amet dolore tempor esse ea Lorem commodo eiusmod. Sit consequat cupidatat aliquip aute esse culpa. Consequat reprehenderit cillum officia eu officia ipsum proident incididunt do ullamco minim pariatur commodo pariatur. Quis enim reprehenderit minim eiusmod proident.'
+  },{ 
+    title: 'Lord of the Rings',
+    body: 'Ex quis ullamco sunt laboris. Veniam laboris cupidatat duis reprehenderit aliquip aliqua irure veniam sunt enim. Id dolore deserunt eu duis occaecat deserunt voluptate enim culpa magna.'
+  }
+])
